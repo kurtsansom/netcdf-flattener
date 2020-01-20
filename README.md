c23034c87ab5d20b3715cf150e8d90227cb184fb
Guillaume Obrecht <guillaume.obrecht@c-ssystems.de>
2020-01-20 18:50:32 +0100

    Initial structure

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # netcdf-flattener
 
 Flatten netCDF files while preserving references as described in the CF Conventions 1.8, chapter 2.7.
+
+Install dependencies:
+python3 -m pip install --upgrade pip setuptools wheel
+python3 -m pip install tqdm
+python3 -m pip install --user --upgrade twine
+
+Compile as Pip package:
+python3 setup.py bdist_wheel
+
+Install Pip package:
+python3 -m pip install dist/netcdf_flattener-0.1-py3-none-any.whl
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='netcdf-flattener',  
+     version='0.0.1',
+#     scripts=['src/test'] ,
+     author="Guillaume Obrecht",
+     author_email="guillaume.obrecht@c-ssystems.de",
+     description="The NetCDF-flattener package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://gitlab.eumetsat.int/additional-data-services/netcdf-flattener",
+     package_dir={'': 'src'},
+     packages=setuptools.find_packages('src'),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache Software License",
+         "Operating System :: OS Independent",
+     ],
+ )
